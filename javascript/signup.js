60d6afa846997b379f496646c693cce27fc32393
Kamoti <kamotigodfrey2@gmail.com>
2020-07-25 17:43:58 +0300

    ADDED PAGES

---
+++ javascript/signup.js
@@ -0,0 +1,10 @@
+
+// Get the modal
+var modal = document.getElementById('sign1');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
